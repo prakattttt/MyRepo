26a34e170f54339d931bbaabdd19ab3f12935313
Prakat <prakat.dhakall64@gmail.com>
2025-07-04 22:22:27 +0545

    Progress on form fillup section with minor section and responsiveness remaining

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//For main document's navbar
 const sidebar = document.querySelector('.sidebar');
 function showSidebar() {
     sidebar.style.display = "flex";
@@ -6,3 +7,16 @@ function showSidebar() {
 function hideSidebar() {
     sidebar.style.display = "none";
 }
+
+//For form
+const container = document.querySelector('.container');
+const registerBtn = document.querySelector('.registerBtn');
+const loginBtn = document.querySelector('.loginBtn');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+})
