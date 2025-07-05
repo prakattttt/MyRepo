96f3e49c02ad0645577b58296d24d67037fa1a5a
Prakat <prakat.dhakall64@gmail.com>
2025-07-05 18:51:58 +0545

    Added responsiveness to the login and register page

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 })
+
+const login = document.querySelector('.login');
+const register = document.querySelector('.register');
+
+function toggleRegister() {
+    register.style.visibility = "visible";
+    login.style.visibility = "hidden";
+}
+
+function toggleLogin() {
+    login.style.visibility = "visible";
+    register.style.visibility = "hidden";
+}
